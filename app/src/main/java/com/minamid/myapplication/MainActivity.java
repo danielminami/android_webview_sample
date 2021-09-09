38e41255179d0cf0827e9a1aee40d7dbc27261fe
Daniel <danielminami@hotmail.com>
2021-09-09 10:37:44 -0400

    Initial commit

---
+++ app/src/main/java/com/minamid/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.minamid.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        webView.setWebViewClient(new CustomWebview());
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+
+        webView.loadUrl("http://www.google.com");
+    }
+
+    private class CustomWebview extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
